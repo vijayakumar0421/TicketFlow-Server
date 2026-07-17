316b877b0a121af21f102318703f38b4ebe154d2
Vijayakumar M <vijayakumardigipro@gmail.com>
2026-07-17 16:17:09 +0530

    Add Prisma login debug console

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,9 +1,8 @@
 const { PrismaClient } = require("@prisma/client");
 
-console.log("Prisma DATABASE_URL:", process.env.DATABASE_URL);
+console.log("DATABASE_URL FROM PRISMA:");
+console.log(process.env.DATABASE_URL);
 
-const prisma = new PrismaClient({
-  log: ["query", "error", "warn"],
-});
+const prisma = new PrismaClient();
 
 module.exports = prisma;
